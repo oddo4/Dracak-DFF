339f8252106e4f389a1c60d27b234bfc1dc19bde
oddo4 <oddo4@seznam.cz>
2018-02-20 12:48:21 +0100

    New Pages Added

---
+++ MainDFF/MainDFF/MainMenuSelectAction.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace MainDFF
+{
+    public class MainMenuSelectAction : AMenuSelectAction
+    {
+        public Page NavigateToPage = null;
+        public int GetCursor(Key key, int current, int max, Window window = null)
+        {
+            switch (key)
+            {
+                case Key.Up:
+                    return MoveCursor(0, current, max);
+                case Key.Down:
+                    return MoveCursor(1, current, max);
+                /// <summary>
+                /// Window resolution change
+                /// </summary>
+                case Key.NumPad0:
+                    window.Width = 640;
+                    window.Height = 480;
+                    break;
+                case Key.NumPad1:
+                    window.Width = 1024;
+                    window.Height = 768;
+                    break;
+                case Key.Enter:
+                    ConfirmSelection(current);
+                    return -2;
+                default:
+                    return current;
+            }
+
+            return -1;
+        }
+        private void ConfirmSelection(int current)
+        {
+            switch (current)
+            {
+                case 0:
+                    NavigateToPage = new LevelSelectPage();
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ MainDFF/MainDFF/Pages/MainMenuPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MainDFF
+{
+    /// <summary>
+    /// Interakční logika pro MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Page
+    {
+        Window window;
+        MainMenuSelectAction menuAction = new MainMenuSelectAction();
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+        private void MenuKey_Loaded(object sender, RoutedEventArgs e)
+        {
+            window = Window.GetWindow(this);
+            window.KeyDown += MenuKeyDown;
+        }
+        private void MenuKeyDown(object sender, KeyEventArgs e)
+        {
+            var current = Grid.GetRow(MenuCursor);
+            var max = gridMenu.RowDefinitions.Count - 1;
+            var selected = menuAction.GetCursor(e.Key, current, max, window);
+            if (selected > -1)
+            {
+                Grid.SetRow(MenuCursor, selected);
+            }
+            else if (selected == -1)
+            {
+                Debug.WriteLine("E0001");
+            }
+            else
+            {
+                if (menuAction.NavigateToPage != null)
+                {
+                    NavigationService.Navigate(menuAction.NavigateToPage);
+                }
+            }
+        }
+    }
+}
